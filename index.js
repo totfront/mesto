0462263d795a37efb81109414054c4d557ad9df5
Alex Mir <expressprogress@ya.ru>
2021-01-27 20:04:26 +0300

    fix: модальное окно не скроллиться вертикально, добавил nojekill

--- index.js
+++ index.js
@@ -1,35 +1,56 @@
 let popupCloseBtn = document.querySelector('.popup__close-btn')
 let popup = document.querySelector('.popup')
 let editBtn = document.querySelector('.profile__edit-btn')
-let inputName = document.querySelector('.popup__input-name')
-let inputDescription = document.querySelector('.popup__input-description')
+let nameInput = document.querySelector('.popup__input-name')
+let jobInput = document.querySelector('.popup__input-description')
 let profileName = document.querySelector('.profile__name')
 let profileDescription = document.querySelector('.profile__description')
 let saveBtn = document.querySelector('.popup__save-btn')
+let formElement = document.querySelector('.popup__form')
+let likeBtn = document.querySelector('.elements__like-btn')
+let pageEl = document.querySelector('.page')
 
 // Заполняет форму
 let fillInputs = () => {
-  inputName.setAttribute('value', profileName.innerHTML)
-  inputName.value.textContent = profileName.innerHTML
-  inputDescription.setAttribute('value', profileDescription.innerHTML)
+  nameInput.setAttribute('value', profileName.innerHTML)
+  nameInput.value.textContent = profileName.innerHTML
+  jobInput.setAttribute('value', profileDescription.innerHTML)
 }
 
 // Прячет Popup
-let hidePopup = () => {
+let togglePopup = () => {
   popup.classList.toggle('popup__opened')
+  pageEl.style.overflow = 'hidden'
+  fillInputs()
 
   if (popup.classList.contains('popup__opened') == 0) {
-    fillInputs()
+    pageEl.style.overflow = 'auto'
   }
 }
 
 // Заполняет профиль и скрывает Popup
 let fillProfile = () => {
-  profileName.textContent = inputName.value
-  profileDescription.textContent = inputDescription.value
-  hidePopup()
+  profileName.textContent = nameInput.value
+  profileDescription.textContent = jobInput.value
+  togglePopup()
 }
 
-popupCloseBtn.addEventListener('click', hidePopup)
-editBtn.addEventListener('click', hidePopup)
+// Обработчик «отправки» формы (пока не работает)
+function formSubmitHandler(evt) {
+  evt.preventDefault()
+  // Эта строчка отменяет стандартную отправку формы.
+  profileName.textContent = nameInput.value
+  profileDescription.textContent = jobInput.value
+}
+
+// Наполняет сердца цветом (любовью)
+let fillLove = () => {
+  console.log(123)
+}
+
+// Прикрепляем обработчики:
+formElement.addEventListener('submit', formSubmitHandler)
+popupCloseBtn.addEventListener('click', togglePopup)
+editBtn.addEventListener('click', togglePopup)
 saveBtn.addEventListener('click', fillProfile)
+likeBtn.addEventListener('click', fillLove)
