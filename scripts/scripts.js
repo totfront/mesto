6a30360b32b0b61d94bec8d561a2ef43d29c5bf4
Alex Mir <expressprogress@ya.ru>
2021-01-30 17:17:57 +0300

    hotfix: исправить последние правки ревьюера

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,37 +5,33 @@ let nameInput = document.querySelector('.popup__input_name')
 let jobInput = document.querySelector('.popup__input_description')
 let profileName = document.querySelector('.profile__name')
 let profileDescription = document.querySelector('.profile__description')
-let saveBtn = document.querySelector('.popup__save-btn')
-let formElement = document.querySelector('.popup__form')
-// Заполняет форму
-let fillInputs = () => {
-  nameInput.setAttribute('value', profileName.innerHTML)
-  nameInput.value = profileName.innerHTML
-  jobInput.setAttribute('value', profileDescription.innerHTML)
+let form = document.querySelector('.popup__form')
+// Показывает popup
+let showPopup = () => {
+  if (popup.classList.contains('popup_opened') == 0)  {
+    popup.classList.add('popup_opened')
+  }
+  // Заполняет поля формы данными со страницы
+  nameInput.setAttribute('value', profileName.textContent)
+  jobInput.setAttribute('value', profileDescription.textContent)
 }
-// Прячет Popup
-let togglePopup = () => {
-  popup.classList.toggle('popup_opened')
-  
-  console.log(popup.classList.contains('popup_opened'));
-  fillInputs()
-}
-// Заполняет профиль и скрывает Popup
-let fillProfile = () => {
+// Прячет попап и заполняет профиль новыми данными
+let hidePopup = () => {
+  if (popup.classList.contains('popup_opened')) {
+    popup.classList.remove('popup_opened')
+  }
+  // Переписывает данные на странице введенными в форму
   profileName.textContent = nameInput.value
   profileDescription.textContent = jobInput.value
-  console.log(123);
-  togglePopup()
 }
 // Обработчик «отправки» формы (пока не работает)
 function formSubmitHandler(evt) {
   evt.preventDefault()
-  // Эта строчка отменяет стандартную отправку формы.
   profileName.textContent = nameInput.value
   profileDescription.textContent = jobInput.value
+  hidePopup()
 }
 // Прикрепляем обработчики:
-formElement.addEventListener('submit', formSubmitHandler)
-popupCloseBtn.addEventListener('click', togglePopup)
-editBtn.addEventListener('click', togglePopup)
-saveBtn.addEventListener('click', fillProfile)
+form.addEventListener('submit', formSubmitHandler)
+popupCloseBtn.addEventListener('click', hidePopup)
+editBtn.addEventListener('click', showPopup)
