178238e7f6a8d6cf853de4b3b3c7c306c27f53d4
Alex Mir <expressprogress@ya.ru>
2021-01-31 18:21:08 +0300

    fix: поправить замечания ревьюерки

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,25 +1,21 @@
 let popupCloseBtn = document.querySelector('.popup__close-btn')
 let popup = document.querySelector('.popup')
 let editBtn = document.querySelector('.profile__edit-btn')
-let nameInput = document.querySelector('.popup__input_name')
-let jobInput = document.querySelector('.popup__input_description')
+let nameInput = document.querySelector('.popup__input_data_name')
+let jobInput = document.querySelector('.popup__input_data_job')
 let profileName = document.querySelector('.profile__name')
 let profileDescription = document.querySelector('.profile__description')
 let form = document.querySelector('.popup__form')
 // Показывает popup
 let showPopup = () => {
-  if (popup.classList.contains('popup_opened') == 0)  {
-    popup.classList.add('popup_opened')
-  }
+  popup.classList.add('popup_opened')
   // Заполняет поля формы данными со страницы
   nameInput.setAttribute('value', profileName.textContent)
   jobInput.setAttribute('value', profileDescription.textContent)
 }
 // Прячет попап и заполняет профиль новыми данными
 let hidePopup = () => {
-  if (popup.classList.contains('popup_opened')) {
-    popup.classList.remove('popup_opened')
-  }
+  popup.classList.remove('popup_opened')
   // Переписывает данные на странице введенными в форму
   profileName.textContent = nameInput.value
   profileDescription.textContent = jobInput.value
