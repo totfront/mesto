3b6fbd0fcd80273476c6dc8407c1b688ed0704d7
Alex Mir <expressprogress@ya.ru>
2021-02-24 22:10:45 +0300

    feat: валидация формы

--- scripts/validate.js
+++ scripts/validate.js
@@ -17,31 +17,58 @@ const toggleButtonState = (inputList, buttonElement) => {
   }
   const hasNotValidInput = inputList.some(findTheOneNotValid)
   if (hasNotValidInput) {
+    console.log('Показать ошибку')
     buttonElement.setAttribute('disabled', true)
     buttonElement.classList.add('popup__save-btn_inactive')
   } else {
+    console.log('Скрыть ошибку')
     buttonElement.removeAttribute('disabled')
     buttonElement.classList.remove('popup__save-btn_inactive')
   }
 }
 
+const getErrorMessage = inputElement => {
+  const defaultErrorHandler = inputElement => inputElement.validationMessage
+  const multyErrorHandler = inputElement => {
+    if (inputElement.validity.typeMismatch) {
+      return 'Введите адрес сайта.'
+    }
+    if (inputElement.validity.valueMissing) {
+      return 'Вы пропустили это поле.'
+    }
+    if (inputElement.validity.tooShort) {
+      const minInputValue = inputElement.getAttribute('minlength')
+      return `Минимальное количество символов: ${minInputValue}. Длина текста сейчас: ${inputElement.value.length} символ `
+    }
+  }
+  const errorHandlers = {
+    name: multyErrorHandler,
+    description: multyErrorHandler,
+    url: multyErrorHandler,
+    DEFAULT: defaultErrorHandler
+  }
+
+  const errorHandler = errorHandlers[inputElement.name] || errorHandlers.DEFAULT
+  return errorHandler(inputElement)
+}
+
 const checkInputValidity = (formElement, inputElement) => {
   const isInputNotValid = !inputElement.validity.valid
   if (isInputNotValid) {
-    const errorMessage = inputElement.validationMessage
+    const errorMessage = getErrorMessage(inputElement)
     showInputError(formElement, inputElement, errorMessage)
   } else {
     hideInputError(formElement, inputElement)
   }
 }
 
-const setEventListeners = formElement => {
+const setEventListeners = (formElement, inputSelector) => {
   const handleFormSubmit = event => {
     event.preventDefault()
   }
   formElement.addEventListener('submit', handleFormSubmit)
 
-  const inputList = Array.from(formElement.querySelectorAll('.popup__input'))
+  const inputList = Array.from(formElement.querySelectorAll(inputSelector))
   const submitBtn = formElement.querySelector('.popup__save-btn')
   const inputListIterator = inputElement => {
     const handleInput = event => {
@@ -54,12 +81,15 @@ const setEventListeners = formElement => {
   toggleButtonState(inputList, submitBtn)
 }
 
-const enableValidation = () => {
-  const formElements = document.querySelectorAll('form')
+const enableValidation = ({ formSelector, inputSelector }) => {
+  const formElements = document.querySelectorAll(formSelector)
   const formList = Array.from(formElements)
   formList.forEach(formElement => {
-    setEventListeners(formElement)
+    setEventListeners(formElement, inputSelector)
   })
 }
 
-enableValidation()
+enableValidation({
+  formSelector: 'form',
+  inputSelector: '.popup__input'
+})
